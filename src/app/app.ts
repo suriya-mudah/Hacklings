8878d599201a754ae8efec0d9647fb9616886ca6
suriya-mudah <suriya@mudah.no>
2025-09-02 17:20:16 +0200

    add completed nav component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../shared/button.component';
+import { NavComponent } from '../shared/nav.component';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule, ButtonComponent, NavComponent],
   templateUrl: './login.html',
   styleUrls: ['./login.scss']
 })
